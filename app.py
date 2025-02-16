ab6a2ea8ae1971932424b53d5073550ad48e0297
Arthur <tarabanovartyr59@gmail.com>
2025-02-16 16:07:11 +0200

    1. Added main page index.html with the ability to go to the order creation page
    2. Changed (sorted) the order of the code (functions, routes) in the app.py file

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
-import os, json
-from flask import Flask, render_template, request
 import sqlite3
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/hello/<name>')
+def greet(name='Stranger'):
+    return render_template('greeting.html', name=name)
+
+
+@app.route('/order', methods=['GET', 'POST'])
+def order():
+    if request.method == 'POST':
+        new_order = {
+            'customer_name': request.form['customer_name'],
+            'drink': request.form['drink'],
+            'flavor': request.form['flavor'],
+            'topping': request.form['topping']
+        }
+        save_order(new_order)
+        
+        return render_template('print.html', new_order=new_order)
+        
+    return render_template('order.html', drinks=drinks, flavors=flavors, toppings=toppings)
+
+
+@app.route('/list', methods=['GET'])
+def list():
+    orders = get_orders()
+    
+    return render_template('list.html', orders=orders)
 
 
 def save_order(order):
@@ -41,42 +77,6 @@ toppings = read_menu('toppings.txt')
 con = sqlite3.connect('orders.db')
 cur = con.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS orders(customer_name, drink, flavor, topping)')
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-
-@app.route('/hello/<name>')
-def greet(name='Stranger'):
-    return render_template('greeting.html', name=name)
-
-
-@app.route('/order', methods=['GET', 'POST'])
-def order():
-    if request.method == 'POST':
-        new_order = {
-            'customer_name': request.form['customer_name'],
-            'drink': request.form['drink'],
-            'flavor': request.form['flavor'],
-            'topping': request.form['topping']
-        }
-        save_order(new_order)
-        
-        return render_template('print.html', new_order=new_order)
-        
-    return render_template('order.html', drinks=drinks, flavors=flavors, toppings=toppings)
-
-
-@app.route('/list', methods=['GET'])
-def list():
-    orders = get_orders()
-    
-    return render_template('list.html', orders=orders)
 
 
 if __name__ == '__main__':
